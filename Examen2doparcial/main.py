cc16a301d1c4755e87995b7ece36f15eb6019c1c
EstefaniaAlegriavazquez <alegriaestefania934@gmail.com>
2026-03-10 15:40:20 -0600

    PARTE2_Examen
    Entrega de la segunda parte del examen Estefania Alegria Vazquez

--- Examen2doparcial/main.py
+++ Examen2doparcial/main.py
@@ -2,9 +2,9 @@ import asyncio
 from typing import Optional
 from pydantic import BaseModel,Field
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
 from datetime import datetime, timedelta
+import secrets
+from fastapi import FastAPI, status,HTTPException, Depends
 
 #Instancia del servidor
 app= FastAPI(
@@ -19,38 +19,89 @@ restaurante = [
     {"id": 2, "nombre": "Restaurante_Lupita"},
 ]
 
+
+# Seguridad HTTP BASIC#
+
+security= HTTPBasic()
+def verificar_peticion(credenciales:HTTPBasicCredentials=Depends(security)):
+    usuario_correcto= secrets.compare_digest(credenciales.username,"admin")
+    contrasena_correcta= secrets.compare_digest(credenciales.password,"rest123")
+
+    if not(usuario_correcto and contrasena_correcta):
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail=" Credenciales no validas"
+        )
+    return credenciales.username
+
+
 #Creamos el modelo validación pydantic
 class reserva(BaseModel):
     nombre: str = Field(..., min_length=6, max_length=100)
     numero_personas:int= Field(..., ge=1,le=18,description="Personas aceptadas")
-    fecha: str = Field(..., min_length=8:00, max_length=10:00)
+    fecha: int = Field(..., gt=8:00, max_length=10:00 , le=datetime.now().years)
     año: int = Field(..., gt=1450, le=datetime.now().year)
-    
-#Enpoints
-@app.get("/v1/restaurante/",tags=['HTTP CRUD'])
-# Parámetro obligatorio
-@app.get("/v1/parametroOb/{nombre}", tags=["Parametro Obligatorio"])
-async def consultaid(id: int):
+
+  
+#Endpoints
+@app.get("/")
+async def sistema_reservas():
+    return {"mensaje":"Funciona sistema de reservas"}
+
+
+#Endpoint con parámetro obligatorio
+@app.get("/Reservas/{id}")
+async def obtener_usuario(nombre: str):
     return {
-        "mensaje": "Usuario encontrado",
-        "usuario": id,
-        "status": "200"
+        "mensaje": "Reserva encontrada",
+        "id_usuario": id
     }
 
-#Endpoint para crear reserva
+#Crear reserva
 @app.post("/reservas", status_code=status.HTTP_201_CREATED)
-async def registrar_reserva(libro: Libro):
+async def crear_reserva(reserva: reserva):
 
-    for r in reservas:
-        if l["nombre"].lower() == libro.nombre.lower():
+    for reser in reserva:
+        if reser["nombre_cliente"].lower() == reserva.nombre.lower():
             raise HTTPException(
                 status_code=400,
-                detail="El libro ya existe"
+                detail="La reserva ya existe"
             )
 
-    libros.append(libro.dict())
+    reserva.append(reserva.dict())
 
     return {
-        "mensaje": "Libro registrado correctamente",
+        "mensaje": "Reserva creada correctamente",
         "status": "201"
     }
+
+#Endpoint para las listas de reservas
+@app.get("/reservas", response_model=List[reserva])
+async def listar_libros():
+    return reserva
+
+#Endpoint con parámetro obligatorio
+#Consultar por ID
+@app.get("/reservas/{id}")
+async def obtener_reserva(id: int, usuarioAuth:str= Depends(verificar_peticion)):
+    return {
+        "mensaje": "reserva encontrada",
+        "id_usuario": id
+    }
+
+#DELETE cancelar reserva
+@app.delete("/v1/reserva/{id}", tags=["HTTP CRUD"])
+async def Cancelar_reserva(id: int, usuarioAuth:str= Depends(verificar_peticion)):
+
+    for reser in reserva:
+        if reser["id"] == id:
+            reserva.remove(reser)
+            return {
+                "mensaje": "Reserva eliminada correctamente"
+            }
+
+    raise HTTPException(
+        status_code=404,
+        detail="Reserva no encontrado"
+    )
+
